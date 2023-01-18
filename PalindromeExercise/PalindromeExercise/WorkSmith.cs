bed01c93dc216d92c3f41415bd31f569bb32670c
schavis2022 <schavis2022>
2023-01-18 11:46:22 -0500

    Palindrome Ex.2 Completed and Working

---
+++ PalindromeExercise/PalindromeExercise/WorkSmith.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WorkSmith
+    {        public static bool IsAPalindrome(string word)
+        {
+            //reverse the word //ex. racecar ---7 charaters  --> 6 indexs (0 to 7)
+            var reversed = "";
+
+            for (int i = word.Length - 1;  i >= 0; i--) 
+            {
+                reversed += word[i];
+            }
+
+            //compare the original with the reversed
+            if(reversed.ToLower() == word.ToLower())
+            {
+                return (reversed.ToLower() == word.ToLower());
+            }
+            else
+            {
+                return false; 
+            }
+
+
+            //return whether they are the same or not
+        }
+
+        
+    }
+
+    
+
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,29 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
+    {   // Palindromes are words or phrases that read the same backward and forward, letter for letter, number for number, or word for word.
+
+        [Theory]
+        [InlineData("racecar",  true)]
+        [InlineData("hello", false)]
+        [InlineData("Civic", true)]
+        [InlineData("A nut for a jar of tuna", false)]
+        [InlineData("A man, a plan, a canal – Panama", false)]
+        public void MyTest(string word, bool expected)
         {
+            //arrange
+            var tester = new WorkSmith();
+
+            //act
+            var actual = WorkSmith.IsAPalindrome(word);
+
+            //assert
+            Assert.Equal(expected, actual);
+
 
         }
     }
